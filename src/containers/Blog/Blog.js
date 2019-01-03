55e2a6e488df3ab51880fb0ea8e284c4e65bc3d3
Methodician <jacob@flight.run>
2019-01-03 10:25:56 -0800

    initial commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+    async componentDidMount() {
+        const res = await axios.get('http://jsonplaceholder.typicode.com/posts');
+        console.log(res);
+        this.setState({ posts: res.data });
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <section className="Posts">
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
